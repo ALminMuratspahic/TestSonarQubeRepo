2dc76d7c2622a824c049853491e57e9677c7e24f
AlminMuratspahic89 <131963557+AlminMuratspahic89@users.noreply.github.com>
2024-07-24 11:32:22 +0200

    init

---
+++ TestSonarQubeAn/src/main/java/com/TestSonarQubeAn/TestSonarQubeAnApplication.java
@@ -0,0 +1,13 @@
+package com.TestSonarQubeAn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSonarQubeAnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSonarQubeAnApplication.class, args);
+	}
+
+}
---
+++ TestSonarQubeAn/src/test/java/com/TestSonarQubeAn/TestSonarQubeAnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TestSonarQubeAn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSonarQubeAnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
